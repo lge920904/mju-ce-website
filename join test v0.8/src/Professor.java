6af29d9fe06c245bb09f3dad9d9a94b7a72ad236
leegaeul <lge920904@naver.com>
2014-01-09 16:30:55 +0900

    add sql

---
+++ join test v0.8/src/Professor.java
@@ -0,0 +1,26 @@
+import javax.swing.ImageIcon;
+
+
+public class Professor extends User {
+	private String major;
+
+	public Professor(){
+		
+	}
+	public Professor(String id, String password, String name, String email, ImageIcon photo, int authority, String major){
+		super(id, password, name, email, photo, authority);
+		this.major = major;
+	}
+	public String getMajor() {
+		return major;
+	}
+
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	
+	public Professor getProfessor(){
+		return this;
+	}
+
+}
